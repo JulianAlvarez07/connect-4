120a84638db4dd6c34f4865fe95cce7f7ef52a74
JulianAlvarez07 <alvarezjuli30@gmail.com>
2025-04-11 00:27:09 -0300

    game finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Connect4 from './components/Connect4'
 const App = () => {
   return (
     <>
-      <h1 className='text-center text-8xl title mt-8'>Connect 4</h1>
+      <h1 className='text-center text-7xl title mt-8'>Connect 4</h1>
       <Connect4 />
     </>
   )
